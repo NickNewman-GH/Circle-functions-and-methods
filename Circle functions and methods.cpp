540ba932835edfdc296031525b75349ea3c17f8a
NickNewman-GH <57058855+NickNewman-GH@users.noreply.github.com>
2021-02-17 14:13:14 +0800

    Add files via upload

---
+++ Circle functions and methods.cpp
@@ -0,0 +1,123 @@
+﻿#include <iostream>
+#include <vector>
+
+struct Circle {
+    float x, y, radius;
+    char label;
+
+    // constructor
+    Circle(float x_, float y_, float radius_, char label_) {
+        x = x_;
+        y = y_;
+        radius = radius_;
+        label = label_;
+    }
+    // destructor
+    ~Circle() {
+    }
+
+    float from_origin() {
+        return sqrt(x * x + y * y);
+    }
+
+    float to_second_center(Circle c) {
+        return sqrt(pow(abs(x - c.x), 2) + pow(abs(y - c.y), 2));
+    }
+    
+    float to_X(){
+        if (abs(y) > radius) {
+            if (y > 0)
+                return y - radius;
+            else
+                return y + radius;
+        }
+        else
+            return 0;
+    }
+
+    float from_circle() {
+        return sqrt(x * x + y * y) - radius;
+    }
+
+    bool can_be_unside(Circle c) {
+        if (radius < c.radius)
+            return true;
+        else
+            return false;
+    }
+
+    bool is_circles_cross(Circle c) {
+        if (sqrt(pow(abs(x - c.x), 2) + pow(abs(y - c.y), 2)) >= radius + c.radius)
+            return false;
+        else
+            return true;
+    }
+};
+
+float from_origin(Circle c) {
+    return sqrt(c.x * c.x + c.y * c.y);
+}
+
+float to_second_center(Circle c, Circle c1) {
+    return sqrt(pow(abs(c.x - c1.x), 2) + pow(abs(c.y - c1.y), 2));
+}
+
+float to_X(Circle c) {
+    if (abs(c.y) > c.radius) {
+        if (c.y > 0)
+            return c.y - c.radius;
+        else
+            return c.y + c.radius;
+    }
+    else
+        return 0;
+}
+
+float from_circle(Circle c) {
+    return sqrt(c.x * c.x + c.y * c.y) - c.radius;
+}
+
+bool can_be_unside(Circle c, Circle c1) {
+    if (c.radius < c1.radius)
+        return true;
+    else
+        return false;
+}
+
+bool is_circles_cross(Circle c, Circle c1) {
+    if (sqrt(pow(abs(c.x - c1.x), 2) + pow(abs(c.y - c1.y), 2)) >= c.radius + c1.radius)
+        return false;
+    else
+        return true;
+}
+
+//--TO-DO--- Функция отражения по Y, Отразить все окружности из нижней полуплоскости в верхнюю
+
+//void y_reflect(std::vector<Circle> mass) {
+//    auto it = mass.begin();
+//    while (it != mass.end()) {
+//        mass[0] = *it;
+//        ++it;
+//    }
+//}
+
+int main()
+{
+    Circle c(10, 20, 2, 'a');
+    Circle c1(5, 40, 3, 'b');
+    Circle c2(-20, 10, 4, 'c');
+    std::vector<Circle> mass = { c, c1, c2 };
+//    y_reflect(mass);
+    std::cout << c.from_origin() << std::endl;
+    std::cout << from_origin(c) << std::endl;
+    std::cout << c.to_second_center(c1) << std::endl;
+    std::cout << to_second_center(c, c1) << std::endl;
+    std::cout << c.to_X() << std::endl;
+    std::cout << to_X(c) << std::endl;
+    std::cout << c.from_circle() << std::endl;
+    std::cout << from_circle(c) << std::endl;
+    std::cout << c.can_be_unside(c1) << std::endl;
+    std::cout << can_be_unside(c,c1) << std::endl;
+    std::cout << c.is_circles_cross(c1) << std::endl;
+    std::cout << is_circles_cross(c, c1) << std::endl;
+}
