869c786af9e7ea64249970bbef52f4ad57a21eab
NickNewman-GH <57058855+NickNewman-GH@users.noreply.github.com>
2021-02-17 19:35:15 +0800

    Add files via upload

--- Circle functions and methods.cpp
+++ Circle functions and methods.cpp
@@ -93,31 +93,57 @@ bool is_circles_cross(Circle c, Circle c1) {
 
 //--TO-DO--- Функция отражения по Y, Отразить все окружности из нижней полуплоскости в верхнюю
 
-//void y_reflect(std::vector<Circle> mass) {
-//    auto it = mass.begin();
-//    while (it != mass.end()) {
-//        mass[0] = *it;
-//        ++it;
-//    }
-//}
+void y_reflect(std::vector<Circle> &mass) {
+    auto it = mass.begin();
+    while (it != mass.end()) {
+        it->y = -it->y;
+        ++it;
+    }
+}
+
+void circles_up(std::vector<Circle>& mass) {
+    auto it = mass.begin();
+    while (it != mass.end()) {
+        it->y = abs(it->y);
+        ++it;
+    }
+}
 
 int main()
 {
     Circle c(10, 20, 2, 'a');
     Circle c1(5, 40, 3, 'b');
     Circle c2(-20, 10, 4, 'c');
-    std::vector<Circle> mass = { c, c1, c2 };
-//    y_reflect(mass);
+    
     std::cout << c.from_origin() << std::endl;
-    std::cout << from_origin(c) << std::endl;
     std::cout << c.to_second_center(c1) << std::endl;
-    std::cout << to_second_center(c, c1) << std::endl;
     std::cout << c.to_X() << std::endl;
-    std::cout << to_X(c) << std::endl;
     std::cout << c.from_circle() << std::endl;
-    std::cout << from_circle(c) << std::endl;
     std::cout << c.can_be_unside(c1) << std::endl;
-    std::cout << can_be_unside(c,c1) << std::endl;
     std::cout << c.is_circles_cross(c1) << std::endl;
+
+    std::cout << "-----------------------------" << std::endl;
+
+    std::cout << from_origin(c) << std::endl;
+    std::cout << to_second_center(c, c1) << std::endl;
+    std::cout << to_X(c) << std::endl;
+    std::cout << from_circle(c) << std::endl;
+    std::cout << can_be_unside(c, c1) << std::endl;
     std::cout << is_circles_cross(c, c1) << std::endl;
+
+    std::cout << "-----------------------------" << std::endl;
+
+    std::vector<Circle> mass1 = { c, c1, c2 };
+    std::cout << mass1[0].y << ", " << mass1[1].y << ", " << mass1[2].y << ", " << std::endl;
+    y_reflect(mass1);
+    std::cout << mass1[0].y << ", " << mass1[1].y << ", " << mass1[2].y << ", " << std::endl;
+
+    std::cout << "-----------------------------" << std::endl;
+
+    c1.y = -10;
+
+    std::vector<Circle> mass2 = { c, c1, c2 };
+    std::cout << mass2[0].y << ", " << mass2[1].y << ", " << mass2[2].y << ", " << std::endl;
+    circles_up(mass2);
+    std::cout << mass2[0].y << ", " << mass2[1].y << ", " << mass2[2].y << ", " << std::endl;
 }
